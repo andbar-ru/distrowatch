31b33621e9f3649c9d0ff2226148893078e7d77e
andbar-ru <wind29121982@gmail.com>
2019-08-22 14:55:58 +0300

    show/web

---
+++ show/web/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"math"
+	"net/http"
+
+	"github.com/andbar-ru/distrowatch/show"
+)
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Distr appends some fields to show.Distr.
+type Distr struct {
+	*show.Distr
+	Number        int
+	Leader        bool
+	Newest        bool
+	Oldest        bool
+	LastUpdateStr string
+}
+
+// Data is the data for the template.
+type Data struct {
+	Coords string
+	Distrs []*Distr
+}
+
+func index(writer http.ResponseWriter, request *http.Request) {
+	coords, err := show.GetCoords()
+	check(err)
+	data := Data{}
+	data.Coords = fmt.Sprintf("Coordinates: %.4f (%+.4f), %.4f (%+.4f)", coords.Latitude, coords.LatitudeDelta, coords.Longitude, coords.LongitudeDelta)
+
+	distrs, err := show.GetDistrs()
+	check(err)
+	tmplDistrs := make([]*Distr, 0, len(distrs))
+	var newestLastUpdate, oldestLastUpdate int
+	oldestLastUpdate = 1e8 // certainly greater than any date
+	for i, distr := range distrs {
+		if distr.LastUpdate > newestLastUpdate {
+			newestLastUpdate = distr.LastUpdate
+		}
+		if distr.LastUpdate < oldestLastUpdate {
+			oldestLastUpdate = distr.LastUpdate
+		}
+		d := &Distr{Distr: distr}
+		d.Number = i + 1
+		lastUpdateStr := fmt.Sprint(distr.LastUpdate)
+		d.LastUpdateStr = fmt.Sprintf("%s-%s-%s", lastUpdateStr[:4], lastUpdateStr[4:6], lastUpdateStr[6:8])
+		tmplDistrs = append(tmplDistrs, d)
+	}
+
+	// Difference between adjacent distrs to define leaders.
+	diff := int(math.Ceil(float64(distrs[0].Count) / 10))
+
+	// Define leaders from the end.
+	lastLeaderIndex := -1 // no leaders by default
+	for i := len(distrs) - 1; i > 0; i-- {
+		if distrs[i-1].Count-distrs[i].Count >= diff {
+			lastLeaderIndex = i - 1
+			break
+		}
+	}
+
+	for i, distr := range tmplDistrs {
+		if distr.LastUpdate == newestLastUpdate {
+			distr.Newest = true
+		}
+		if distr.LastUpdate == oldestLastUpdate {
+			distr.Oldest = true
+		}
+		if i <= lastLeaderIndex {
+			distr.Leader = true
+		}
+	}
+	data.Distrs = tmplDistrs
+
+	t := template.Must(template.New("index").ParseFiles("index.html"))
+
+	err = t.Execute(writer, data)
+	check(err)
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
+}
