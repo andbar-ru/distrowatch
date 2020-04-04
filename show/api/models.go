9abbcae586416a642d4dd7418a30148fcaa5117a
Andrei Bartashevich <andrei.bartashevich@krontech.ru>
2020-04-04 19:46:22 +0300

    MarshalJSON for LastUpdate

--- show/api/models.go
+++ show/api/models.go
@@ -1,8 +1,24 @@
 package main
 
+import (
+	"encoding/json"
+	"strconv"
+	"time"
+)
+
+type dateInt int
+
 // Distr represents db table "distrs".
 type Distr struct {
-	Name       string `json:"name"`
-	Count      int    `json:"count"`
-	LastUpdate int    `db:"last_update" json:"last_update"`
+	Name       string  `json:"name"`
+	Count      int     `json:"count"`
+	LastUpdate dateInt `db:"last_update" json:"lastUpdate"`
+}
+
+func (d dateInt) MarshalJSON() ([]byte, error) {
+	date, err := time.Parse("20060102", strconv.Itoa(int(d)))
+	if err != nil {
+		return nil, err
+	}
+	return json.Marshal(date.Format("2006-01-02"))
 }
